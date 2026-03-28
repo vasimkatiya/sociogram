26442f24c0431b7315634e3ca7ae40772c931144
vasim katiya <vasimkatiya97@gmail.com>
2026-03-29 00:43:25 +0530

    fix bugs

--- services/ImageKit.js
+++ services/ImageKit.js
@@ -1,11 +1,14 @@
 const ImageKit = require('@imagekit/nodejs');
 
 const client = new ImageKit({
-    privateKey: "private_Y4czbRp1f62gyrapdc54ZpH5TnY="
+    privateKey: "private_Y4czbRp1f62gyrapdc54ZpH5TnY=",
+    publicKey:"public_bWb/JpRLe9Nq29ytA80XkHKuJT0=",
+    urlEndpoint:"https://ik.imagekit.io/pba1fz17w"
+    
 });
 
 exports.uploadFiles = async (file) =>{
-    const result = await client.files.upload({
+    const result = await client.upload({
         file,
         fileName:"posts" + Date.now(),
     });
